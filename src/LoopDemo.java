47a5eb110371b69d5d132ee2cb03e24dcf7fbde5
Laura <lauraxwitherspoon@gmail.com>
2017-09-01 14:48:54 -0400

    Initial commit test

---
+++ src/LoopDemo.java
@@ -0,0 +1,14 @@
+/**
+ *  Program to demonstrate how loops (iterations) work in Java
+ *  There are 3 types of loops:
+ *  1. For
+ *  2. While
+ *  3. Do While
+ *  @author Laura Witherspoon
+ */
+
+public class LoopDemo {
+
+    // empty class for demonstration purposes
+
+}
